8fc6a5c33c313feeed971d817036dcafb141c9fc
Tweenty <tweenty.pro@gmail.com>
2024-07-20 14:39:31 +0200

    edit: stellia create app command

--- src/cli/create-stellia-app.js
+++ src/cli/create-stellia-app.js
@@ -1,4 +1,4 @@
-import { findUp } from "find-up";
+import { findUp, pathExists } from "find-up";
 import { cpSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,14 +6,28 @@ import { fileURLToPath } from "url";
 export async function createStelliaApp() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const templateDirectory = path.join(__dirname, "../templates/bot");
-    const packageJsonPath = await findUp('package.json');
+
+    const templateDirectoryPath = await findUp(async (directory) => {
+        const completePath = path.join(directory, "bot");
+        const isValidPath = await pathExists(completePath);
+        if (isValidPath) {
+            return completePath;
+        }
+        return false;
+    }, { type: "directory", cwd: path.join(__dirname, "../templates") });
+    if (!templateDirectoryPath) {
+        console.error("Error: Could not find template directory");
+        process.exit(1);
+    }
+
+    const packageJsonPath = await findUp("package.json");
     if (!packageJsonPath) {
         console.error("Error: Could not find package.json");
         process.exit(1);
     }
     const projectRootDirectory = path.dirname(packageJsonPath);
-    cpSync(templateDirectory, projectRootDirectory, { recursive: true });
+
+    cpSync(templateDirectoryPath, projectRootDirectory, { recursive: true });
 
     console.log("Project successfully created! 🚀");
 }
